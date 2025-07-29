7c556db0f00c5e13b2639cb15868d9b177562313
cplm1995 <cristianpalacios193@gmail.com>
2025-07-28 20:00:37 -0500

    Agregando los archivos necesarios

---
+++ app.js
@@ -0,0 +1,49 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    let amigoInput = document.getElementById('amigo');
+    let nombreAmigo = amigoInput.value;
+
+    // 1. Verificar que el input no esté vacío
+    if (nombreAmigo.trim() === '') {
+        alert('Por favor, ingresa el nombre de tu amigo.');
+        return; // Detiene la ejecución de la función
+    }
+
+    // 2. Verificar que el amigo no esté ya en la lista
+    if (amigos.includes(nombreAmigo)) {
+        alert('Este amigo ya ha sido agregado.');
+        limpiarInput(); // Limpiar para el siguiente nombre
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    // Opcional: Actualizar la lista de amigos en la pantalla
+    let lista = document.getElementById('listaAmigos');
+    if (lista) {
+        lista.innerHTML = ""; // Limpiar la lista"
+
+        for (let i = 0; i < amigos.length; i++) {
+            lista.innerHTML += `<li>${amigos[i]}</li>`;
+        }
+    }
+    limpiarInput();
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Debes agregar al menos 2 amigos para poder sortear.');
+        return;
+    }
+    let sorteo = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[sorteo];
+    document.getElementById('listaAmigos').innerHTML = amigoSorteado;
+
+}
+
+function limpiarInput(){
+    document.getElementById('amigo').value = '';
+}
